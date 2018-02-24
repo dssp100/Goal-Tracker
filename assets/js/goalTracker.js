f99333f61094a5033a123b8feb705c9f6c32ec14
Daniel Springer <dssp100@gmail.com>
2018-02-24 11:25:02 +0100

    started with the check circles colors

--- assets/js/goalTracker.js
+++ assets/js/goalTracker.js
@@ -1,1 +1,13 @@
-alert("conected");
+//Color circles
+
+$("li").click(function(){
+  $(this).toggleClass("completed");
+  // if($(this).css("color") === "rgb(128, 128, 128)")
+  //   {
+  //   $(this).css("color", "Black")
+  //   }
+  //   else
+  //     {
+  //       $(this).css("color", "gray")
+  //     }
+});
